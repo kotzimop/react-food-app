37d9712bd0cfe59d5cdf6dd48c01d0384b6e5805
kotzimop <kotzimop@gmail.com>
2023-05-08 10:44:40 +0300

    Adding a Cart Context

---
+++ src/store/CartProvider.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import CartContext from "./cart-context"
+
+const CartProvider = (props) => {
+    const addItemToCartHandler = item => {
+
+    }
+
+    const removeItemFromCartHandler = item => {}
+
+    const cartContext = {
+        items: [],
+        totalAmount: 0,
+        addItem: addItemToCartHandler,
+        removeItem: removeItemFromCartHandler,
+
+    }
+
+  return (
+    <CartContext.Provider value={cartContext}>
+        {props.children}
+    </CartContext.Provider>
+  )
+}
+
+export default CartProvider
