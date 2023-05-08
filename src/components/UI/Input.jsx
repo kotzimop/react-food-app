8f210cf3f736511a0befa63da6622f250ea40d84
kotzimop <kotzimop@gmail.com>
2023-05-08 09:47:22 +0300

    Adding a Form

---
+++ src/components/UI/Input.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import classes from './Input.module.css'
+
+const Input = (props) => {
+  return (
+    <div className={classes.input}>
+        <label htmlFor={props.input.id}>{props.label}</label>
+        <input {...props.input}/>
+    </div>
+  )
+}
+
+export default Input
