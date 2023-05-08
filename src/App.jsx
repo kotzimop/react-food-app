33ecbcd40c2b1b336f64965ef5734f3a164a269e
kotzimop <kotzimop@gmail.com>
2023-05-08 08:57:20 +0300

    Adding the Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+/* eslint-disable react/no-unescaped-entities */
+import { Fragment } from "react";
+import Header from "./components/Layout/Header";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
 }
 
 export default App;
